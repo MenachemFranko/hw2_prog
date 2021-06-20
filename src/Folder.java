2182f08fa63fba25d3f90b64aac81eb725d60627
Menachem Franko <franko@campus.technion.ac.il>
2021-06-20 20:57:56 +0300

    mlelef

--- src/Folder.java
+++ src/Folder.java
@@ -7,7 +7,14 @@ public class Folder extends StorageItem{
     public Folder(String name) {
         super(name);
         this.content = new ArrayList<StorageItem>();
-
+    }
+    public boolean addItem(StorageItem item){
+        for(StorageItem storageItem : this.content){
+            if(storageItem.getName() == item.getName())
+                return false;
+        this.content.add(item);
+        item.setLocation(this);
+        }
     }
 
 
