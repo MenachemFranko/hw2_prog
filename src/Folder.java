6a20fc4741bcb4bdfd89bd583b76406a0a62f4ba
crab-a <crab@notmailinator.com>
2021-06-20 21:07:55 +0300

    setSize getSize for Folder

--- src/Folder.java
+++ src/Folder.java
@@ -10,24 +10,21 @@ public class Folder extends StorageItem{
     }
     public boolean addItem(StorageItem item){
         for(StorageItem storageItem : this.content){
-            if(storageItem.getName() == item.getName())
+            if(storageItem.getName().equals(item.getName()))
                 return false;
         this.content.add(item);
         item.setLocation(this);
         }
+        return true;
     }
-
-    void setSize() {
+    @Override
+    public int getSize() {
         size=0;
         for(StorageItem storageItem : content){
             size += storageItem.getSize();//unsure if need some kind of casting here
         }
         super.setSize(size);
+        return size;
 
-    }
-
-    @Override
-    public int getSize() {
-        return super.getSize();
     }
 }
