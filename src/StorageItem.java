32d5e43dbadc3f14ae408c844d2069dc6d5e2cef
Menachem Franko <franko@campus.technion.ac.il>
2021-06-20 22:10:24 +0300

    mlelef

--- src/StorageItem.java
+++ src/StorageItem.java
@@ -1,21 +1,26 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 abstract class StorageItem {
+    static ArrayList<StorageItem> pc = new ArrayList<StorageItem>();
     final static long LOW_BOUND = 1483228800000L;
     final static long HIGH_BOUND = 1640908800000L;;
     private String name;
     private Date date;
     int size;
-    private Folder location;               // i think we need this for printing, or maybe location?
+    private ArrayList<StorageItem> location;               // i think we need this for printing, or maybe location?
     public StorageItem(String name){
         this.name = name;
         makeData(Main.rnd.nextLong());
         this.size = 0;
+        this.location = pc;
+        pc.add(this);
     }
     public Date getDate(){return this.date};
-    public void setLocation(Folder folder){
-        this.location = folder;
+    public void setLocation(ArrayList<StorageItem> location){
+        this.location = location;
     }
+    public ArrayList<StorageItem> getLocation(){return this.location};
     void makeData(long rndTime){
         if(rndTime<0){
             rndTime=-rndTime;
